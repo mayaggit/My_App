d23bbe940b1c10217007fea2aaa2b989ed0b351d
edib <edibersh@gmail.com>
2019-12-04 16:30:50 +0200

    fixed recyler UI problem still ugly

--- app/src/main/java/com/my_app/MainActivity.java
+++ app/src/main/java/com/my_app/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 import java.util.List;
@@ -16,12 +18,15 @@ public class MainActivity extends AppCompatActivity {
     private ListDAO listDao;
     private List<NameIdPair> groceries;
     private GroceriesAdapter adapter;
+    private View addGrocery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        addGrocery =  findViewById(R.id.add_grocery_button);
 
         listDao = new ListDAO();
         groceries = listDao.getGroceryNames();
@@ -33,6 +38,15 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new GroceriesAdapter(groceries);
         recyclerView.setAdapter(adapter);
+
+        addGrocery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, AddGroceryActivity.class);
+//                myIntent.putExtra("key", value); //Optional parameters
+                startActivity(myIntent);
+            }
+        });
 
     }
 }
